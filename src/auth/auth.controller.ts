3697f4a9fcc040c2b5117d7297dc223836907d9b
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-10-23 17:02:14 -0300

    Adds auth token config

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { NATS_SERVICE } from 'src/config';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -34,16 +45,19 @@ export class AuthController {
     }
   }
 
+  @UseGuards(AuthGuard)
   @Get('verify')
-  async verifyUser() {
-    try {
+  async verifyUser(@User() user: CurrentUser, @Token() token: string) {
+    /* try {
       const res = await firstValueFrom(
-        this.client.send('auth.verify.user', {}),
+        this.client.send('auth.verify.user', { user }),
       );
 
       return res;
     } catch (error) {
       throw new RpcException(error);
-    }
+    } */
+
+    return { user, token };
   }
 }
---
+++ src/auth/decorators/user.decorator.ts
@@ -0,0 +1,16 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+
+    if (!request.user) {
+      throw new InternalServerErrorException('User not found in request');
+    }
+    return request.user;
+  },
+);
